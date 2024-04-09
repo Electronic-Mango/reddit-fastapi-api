d7c01d2592aab54988bc3f2a7676d51ea94843aa
Electronic Mango <78230210+Electronic-Mango@users.noreply.github.com>
2024-04-09 23:33:10 +0200

    Rework type hints in routes
    Now they should correctly show in swagger as a list of possible values, not raw strings.

--- src/api/routes.py
+++ src/api/routes.py
@@ -1,4 +1,4 @@
-from enum import Enum, auto
+from enum import StrEnum, auto
 
 from fastapi import APIRouter
 from redditpythonapi import Article, ArticlesSortTime, ArticlesSortType
@@ -15,21 +15,21 @@ subreddit_router = APIRouter(prefix="/subreddit")
 user_router = APIRouter(prefix="/user")
 
 
-class ArticleType(Enum):
+class ArticleType(StrEnum):
     """Enum with all viable types of loaded articles"""
 
-    ALL = auto()
-    MEDIA = auto()
-    TEXT = auto()
+    ALL = "all"
+    MEDIA = "media"
+    TEXT = "text"
 
 
 @subreddit_router.get("/list/{subreddit}")
 async def subreddit_list(
     subreddit: str,
-    sort: ArticlesSortType | None = None,
-    time: ArticlesSortTime | None = None,
-    count: int | None = None,
-    article_type: ArticleType = ArticleType.ALL,
+    sort: ArticlesSortType = None,
+    time: ArticlesSortTime = None,
+    count: int = None,
+    article_type: ArticleType = None,
 ):
     articles = await get_subreddit_articles(subreddit, sort, time, count)
     filtered_articles = _filter_articles(articles, article_type)
@@ -39,10 +39,10 @@ async def subreddit_list(
 @subreddit_router.get("/random/{subreddit}")
 async def subreddit_random(
     subreddit: str,
-    sort: ArticlesSortType | None = None,
-    time: ArticlesSortTime | None = None,
-    count: int | None = None,
-    article_type: ArticleType = ArticleType.ALL,
+    sort: ArticlesSortType = None,
+    time: ArticlesSortTime = None,
+    count: int = None,
+    article_type: ArticleType = None,
 ):
     articles = await get_subreddit_articles(subreddit, sort, time, count)
     filtered_articles = _filter_articles(articles, article_type)
@@ -52,10 +52,10 @@ async def subreddit_random(
 @user_router.get("/list/{username}")
 async def user_list(
     username: str,
-    sort: ArticlesSortType | None = None,
-    time: ArticlesSortTime | None = None,
-    count: int | None = None,
-    article_type: ArticleType = ArticleType.ALL,
+    sort: ArticlesSortType = None,
+    time: ArticlesSortTime = None,
+    count: int = None,
+    article_type: ArticleType = None,
 ):
     articles = await get_user_articles(username, sort, time, count)
     filtered_articles = _filter_articles(articles, article_type)
@@ -65,17 +65,17 @@ async def user_list(
 @user_router.get("/random/{username}")
 async def user_random(
     username: str,
-    sort: ArticlesSortType | None = None,
-    time: ArticlesSortTime | None = None,
-    count: int | None = None,
-    article_type: ArticleType = ArticleType.ALL,
+    sort: ArticlesSortType = None,
+    time: ArticlesSortTime = None,
+    count: int = None,
+    article_type: ArticleType = None,
 ):
     articles = await get_user_articles(username, sort, time, count)
     filtered_articles = _filter_articles(articles, article_type)
     return prepare_random_response_or_abort(filtered_articles)
 
 
-def _filter_articles(articles: list[Article], article_type: ArticleType) -> list[Article]:
+def _filter_articles(articles: list[Article], article_type: ArticleType | None) -> list[Article]:
     match article_type:
         case ArticleType.MEDIA:
             return filter_media_articles(articles)
