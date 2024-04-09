523e907f2696ceb187f46c636d688f3b9b179584
Electronic Mango <78230210+Electronic-Mango@users.noreply.github.com>
2024-04-09 23:35:43 +0200

    Remove unused fields from YAML configuration

--- src/settings.py
+++ src/settings.py
@@ -40,8 +40,6 @@ API_HOST = _load_config("api", "host")
 API_PORT = _load_config("api", "port")
 API_AUTHORIZATION_HEADER_NAME = _load_config("api", "authorization_header", "name")
 API_AUTHORIZATION_HEADER_VALUE = _load_config("api", "authorization_header", "expected_value")
-DEFAULT_LOAD_COUNT = int(_load_config("api", "reddit", "default_load_count"))
-DEFAULT_SUBREDDIT = _load_config("api", "reddit", "default_subreddit")
 
 REDDIT_CLIENT_ID = _load_config("reddit", "client", "id")
 REDDIT_CLIENT_SECRET = _load_config("reddit", "client", "secret")
