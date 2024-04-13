2c2378beb38933a0638eadabc646b91f39a9d8fe
Electronic Mango <78230210+Electronic-Mango@users.noreply.github.com>
2024-04-13 14:16:29 +0200

    Add missing docstrings in "security" module

--- src/api/security.py
+++ src/api/security.py
@@ -13,9 +13,14 @@ api_key_header = APIKeyHeader(name=API_AUTHORIZATION_HEADER_NAME or "")
 
 
 def verify_api_key(api_key_header_value: str = Security(api_key_header)):
+    """Verify if received API key header value matches expected one.
+
+    Return HTTP code 401 if it's missing or invalid.
+    """
     if not compare_digest(api_key_header_value, API_AUTHORIZATION_HEADER_VALUE):
         raise HTTPException(status.HTTP_401_UNAUTHORIZED, "Missing or invalid API key")
 
 
 def security_enabled() -> bool:
+    """Check if authorization is enabled."""
     return API_AUTHORIZATION_HEADER_NAME and API_AUTHORIZATION_HEADER_VALUE
